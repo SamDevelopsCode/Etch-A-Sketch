6e2db8b8d25c76ca1d7a094dc33969af7e5d4bb8
samdevelopscode <fousek.sam@gmail.com>
2023-03-30 13:41:03 -0500

    Added functionality for squares to be placed in rows and event listeners change background color

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,37 @@
 const gridContainer = document.getElementById("container");
 
+
+
 function createGrids()
 {
-    for (let i = 0; i < 32; i++) 
+    for (let i = 0; i < 3200; i++) 
     {
-        // creates the squares and childs them to container
+        if ((i % 64) == 0)
+        {
+            var rowContainer = document.createElement("div");
+            rowContainer.classList.add("row-container");
+            gridContainer.appendChild(rowContainer);
+        }
+
         var div = document.createElement("div");
-        div.classList.add("grid-element")
-        gridContainer.appendChild(div);     
+        div.classList.add("grid-element");
+        rowContainer.appendChild(div);
     }
 }
 
+function assignEventsToGridElements()
+{
+    var gridElements = document.querySelectorAll("div.grid-element");
+
+    gridElements.forEach((gridElement) => {
+        gridElement.addEventListener("mouseover", () => {
+            gridElement.classList.add("change-background-color");
+        });
+    });
+
+    // we use the .forEach method to iterate through each
+    
+}
+
 createGrids();
+assignEventsToGridElements();
